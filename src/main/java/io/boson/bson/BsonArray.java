c5c78d7f502b2fc2bd90c60dbf67527d15a424e0
Tiago Filipe <tfilipe@growin.pt>
2017-11-17 15:18:42 +0000

    added encodeToBarray() in BsonObject and BsonArray classes

--- src/main/java/io/boson/bson/BsonArray.java
+++ src/main/java/io/boson/bson/BsonArray.java
@@ -546,6 +546,22 @@ public class BsonArray implements Iterable<Object> {
     }
 
     /**
+     * Encode the BSON array as a Byte Array
+     *
+     * @return the Byte Array
+     */
+    public byte[] encodeToBarray(){
+        try {
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            Bson.encode(list, os);
+            os.flush();
+            return os.toByteArray();
+        } catch (IOException e) {
+            throw new VertxException(e);
+        }
+    }
+
+    /**
      * Encode this BSON object in an output stream
      *
      * @return the string encoding.
