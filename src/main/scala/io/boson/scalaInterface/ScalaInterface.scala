ade16f424bd9cff84e69604936be167c5b51ec15
Tiago Filipe <tfilipe@growin.pt>
2017-11-17 14:36:58 +0000

    Removed external dependencies from both interfaces, fixed tests

--- src/main/scala/io/boson/scalaInterface/ScalaInterface.scala
+++ src/main/scala/io/boson/scalaInterface/ScalaInterface.scala
@@ -1,10 +1,7 @@
 package io.boson.scalaInterface
 
-import java.nio.ByteBuffer
 import io.boson.bsonPath.{Interpreter, Program, TinyLanguage}
 import io.boson.nettybson.NettyBson
-import io.netty.buffer.ByteBuf
-import io.vertx.core.buffer.Buffer
 import scala.collection.mutable.ArrayBuffer
 import io.boson.bsonValue
 
@@ -15,15 +12,6 @@ class ScalaInterface {
 
   def createNettyBson(byteArray: Array[Byte]):NettyBson = {
      new NettyBson(byteArray = Option(byteArray))
-  }
-  def createNettyBson(byteBuf: ByteBuf):NettyBson = {
-    new NettyBson(byteBuf = Option(byteBuf))
-  }
-  def createNettyBson(byteBuffer: ByteBuffer):NettyBson = {
-    new NettyBson(javaByteBuf = Option(byteBuffer))
-  }
-  def createNettyBson(vertxBuffer: Buffer):NettyBson = {
-    new NettyBson(vertxBuff = Option(vertxBuffer))
   }
   def createNettyBson(arrayBuffer: ArrayBuffer[Byte]):NettyBson = {
     new NettyBson(scalaArrayBuf = Option(arrayBuffer))
