9ef3442dbb68fa25229b3936b2653ef767b46262
Tiago Filipe <tfilipe@growin.pt>
2018-02-22 17:37:14 +0000

    New Extractor structure. Int, Bool, Double, Long Extractors.

--- src/main/scala/io/boson/bson/bsonImpl/BosonImpl.scala
+++ src/main/scala/io/boson/bson/bsonImpl/BosonImpl.scala
@@ -10,31 +10,51 @@ import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 import scala.util.{Failure, Success, Try}
 
 
-sealed trait Extractor[T] {
-  def applyFunc(f: (T => Unit), value: T): Unit
-}
-
-object Extractor extends DefaultExtractor {
-  def apply[T](f: T => Extractor[T], v: T): Extractor[T] = {
-     f(v)
-  }
-}
-
-trait DefaultExtractor {
-  implicit object StringExtractor extends Extractor[String] {
-    def applyFunc(f: String => Unit, value: String): Unit =
-      f(value)
-  }
-}
-
-
-sealed trait ExtractorFacade {
-  def toPrimitive[T](f: T => Unit, o: T)(implicit tc: Extractor[T]): Unit
-}
-
-object Transform extends ExtractorFacade {
-  def toPrimitive[T](f: T => Unit, o: T)(implicit tc: Extractor[T]): Unit = tc.applyFunc(f,o)
-}
+//sealed trait Extractor[T] {
+//  def applyFunc(f: (T => Unit), value: T): Unit
+//}
+//
+//object Extractor extends DefaultExtractor {
+//  def apply[T](f: T => Extractor[T], v: T): Extractor[T] = {
+//     f(v)
+//  }
+//}
+//
+//trait DefaultExtractor {
+//
+//  implicit object StringExtractor extends Extractor[String] {
+//    def applyFunc(f: String => Unit, value: String): Unit =
+//      f(value)
+//  }
+//
+//  implicit object IntExtractor extends Extractor[Int] {
+//    def applyFunc(f: Int => Unit, value: Int): Unit =
+//      f(value)
+//  }
+//
+//  implicit object BooleanExtractor extends Extractor[Boolean] {
+//    def applyFunc(f: Boolean => Unit, value: Boolean): Unit =
+//      f(value)
+//  }
+//
+//  implicit object DoubleExtractor extends Extractor[Double] {
+//    def applyFunc(f: Double => Unit, value: Double): Unit =
+//      f(value)
+//  }
+//
+//  implicit object LongExtractor extends Extractor[Long] {
+//    def applyFunc(f: Long => Unit, value: Long): Unit =
+//      f(value)
+//  }
+//}
+//
+//sealed trait ExtractorFacade {
+//  def toPrimitive[T](f: T => Unit, o: T)(implicit tc: Extractor[T]): Unit
+//}
+//
+//object Transform extends ExtractorFacade {
+//  def toPrimitive[T](f: T => Unit, o: T)(implicit tc: Extractor[T]): Unit = tc.applyFunc(f,o)
+//}
 
 case class CustomException(smth: String) extends Exception {
   override def getMessage: String = smth
--- src/test/scala/io/boson/ChainedExtractorsTest.scala
+++ src/test/scala/io/boson/ChainedExtractorsTest.scala
@@ -16,24 +16,13 @@ class ChainedExtractorsTest extends FunSuite{
   private val store = new BsonObject().put("Book", "Scala")
   private val bson = new BsonObject().put("Store", store)
 
-  test("Simple extractor") {
+  test("Simple String extractor") {
 
     val expression: String = ".Store.Book"
     //val future: CompletableFuture[BsValue] = new CompletableFuture[BsValue]()
     val boson: Boson = Boson.extractor(expression, (in: BsSeq) => println(s"I got it: $in"))
     boson.go(bson.encode.getBytes).join()
 
-    //Thread.sleep(1500)
-
-//    val expected: Vector[Any] = Vector("Scala")
-//    val result = future.join().getValue.asInstanceOf[Vector[Any]]
-//    assert(expected.size === result.size)
-//    assertTrue(expected.zip(result).forall{
-//      case (e: Array[Byte], r: Array[Byte]) =>
-//        println(s"e: $e, r: $r, are they equal? ${e.sameElements(r)}")
-//        e.sameElements(r)
-//      case (e, r) => e.equals(r)
-//    })
   }
 
 }
