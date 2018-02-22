32ff0d12c0cfbd24dc83917f70c061d1033711ba
Tiago Filipe <tfilipe@growin.pt>
2018-02-22 17:37:42 +0000

    New Extractor structure. Int, Bool, Double, Long Extractors.

---
+++ src/main/scala/io/boson/bson/bsonImpl/Extractors.scala
@@ -0,0 +1,39 @@
+package io.boson.bson.bsonImpl
+
+sealed trait Extractor[T] {
+  def applyFunc(f: (T => Unit), value: T): Unit
+}
+
+object Extractor extends DefaultExtractor {
+  def apply[T](f: T => Extractor[T], v: T): Extractor[T] = {
+    f(v)
+  }
+}
+
+trait DefaultExtractor {
+
+  implicit object StringExtractor extends Extractor[String] {
+    def applyFunc(f: String => Unit, value: String): Unit =
+      f(value)
+  }
+
+  implicit object IntExtractor extends Extractor[Int] {
+    def applyFunc(f: Int => Unit, value: Int): Unit =
+      f(value)
+  }
+
+  implicit object BooleanExtractor extends Extractor[Boolean] {
+    def applyFunc(f: Boolean => Unit, value: Boolean): Unit =
+      f(value)
+  }
+
+  implicit object DoubleExtractor extends Extractor[Double] {
+    def applyFunc(f: Double => Unit, value: Double): Unit =
+      f(value)
+  }
+
+  implicit object LongExtractor extends Extractor[Long] {
+    def applyFunc(f: Long => Unit, value: Long): Unit =
+      f(value)
+  }
+}
