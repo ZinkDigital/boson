2a772c8dbe2ba80529ba14fa9526d296cc14a858
Margarida Reis <mreis@growin.pt>
2018-06-06 11:55:42 +0100

    BosonJava Code clean up

--- boson-java/src/main/java/io/zink/boson/impl/BosonInjector.java
+++ boson-java/src/main/java/io/zink/boson/impl/BosonInjector.java
@@ -51,7 +51,7 @@ public class BosonInjector<T> implements Boson {
     public CompletableFuture<byte[]> go(byte[] bsonByteEncoding) {
         Option opt = apply(bsonByteEncoding);
         Option e = empty();
-        BosonImpl boson = new BosonImpl(opt, e,e);
+        BosonImpl boson = new BosonImpl();
         CompletableFuture<byte[]> future =
                 CompletableFuture.supplyAsync(() -> {
                     //byte[] res =
@@ -77,7 +77,7 @@ public class BosonInjector<T> implements Boson {
     public CompletableFuture<ByteBuffer> go(ByteBuffer bsonByteBufferEncoding) {
         Option opt = apply(bsonByteBufferEncoding);
         Option e = empty();
-        BosonImpl boson = new BosonImpl(opt, e, e);
+        BosonImpl boson = new BosonImpl();
         CompletableFuture<ByteBuffer> future =
                 CompletableFuture.supplyAsync(() -> {
 //                    byte[] res =  parseInj(boson, anon, expression);
